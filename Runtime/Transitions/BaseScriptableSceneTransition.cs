d5d4e2a36ec536314657715f251af4b1a162099f
Edvinas <edvinas108@gmail.com>
2022-09-28 21:59:45 +0300

    Add a way to hook into transitions

--- Runtime/Transitions/BaseScriptableSceneTransition.cs
+++ Runtime/Transitions/BaseScriptableSceneTransition.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using UnityEngine;
 
 namespace CHARK.ScriptableScenes.Transitions
@@ -9,19 +10,73 @@ namespace CHARK.ScriptableScenes.Transitions
     /// </summary>
     public abstract class BaseScriptableSceneTransition : ScriptableObject
     {
+        #region Public Events
+
+        /// <summary>
+        /// Invoked when <see cref="ShowRoutine"/> is starts.
+        /// </summary>
+        public event Action OnShowEntered;
+
+        /// <summary>
+        /// Invoked when <see cref="ShowRoutine"/> is finishes.
+        /// </summary>
+        public event Action OnShowExited;
+
+        /// <summary>
+        /// Invoked when <see cref="HideRoutine"/> is starts.
+        /// </summary>
+        public event Action OnHideEntered;
+
+        /// <summary>
+        /// Invoked when <see cref="HideRoutine"/> is finishes.
+        /// </summary>
+        public event Action OnHideExited;
+
+        #endregion
+
         #region Public Methods
 
         /// <returns>
         /// Enumerator which transitions into the <see cref="BaseScriptableSceneCollection"/>. Called
         /// before the collection is loaded.
         /// </returns>
-        public abstract IEnumerator ShowRoutine();
+        public IEnumerator ShowRoutine()
+        {
+            OnShowEntered?.Invoke();
+            try
+            {
+                yield return OnShowRoutine();
+            }
+            finally
+            {
+                OnShowExited?.Invoke();
+            }
+        }
 
         /// <returns>
         /// Enumerator which transitions out of the <see cref="BaseScriptableSceneCollection"/>. Called
         /// before the collection is unloaded.
         /// </returns>
-        public abstract IEnumerator HideRoutine();
+        public IEnumerator HideRoutine()
+        {
+            OnHideEntered?.Invoke();
+            try
+            {
+                yield return OnHideRoutine();
+            }
+            finally
+            {
+                OnHideExited?.Invoke();
+            }
+        }
+
+        #endregion
+
+        #region Protected Methods
+
+        protected abstract IEnumerator OnShowRoutine();
+
+        protected abstract IEnumerator OnHideRoutine();
 
         #endregion
     }
