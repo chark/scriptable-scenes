7b08c4f8ea0f437377f8318f48f2a63a60655f0a
Edvinas <edvinas108@gmail.com>
2023-10-03 15:12:21 +0300

    Move samples to Samples~ folder

---
+++ Samples~/ScenesAndCollections/Scripts/MenuCanvasController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace CHARK.ScriptableScenes
+{
+    internal sealed class MenuCanvasController : MonoBehaviour
+    {
+        [Header("Scenes")]
+        [SerializeField]
+        private ScriptableSceneCollection playSceneCollection;
+
+        [Header("Buttons")]
+        [SerializeField]
+        private Button playButton;
+
+        [SerializeField]
+        private Button exitButton;
+
+        private void OnEnable()
+        {
+            playButton.onClick.AddListener(HandlePlayButtonClick);
+            exitButton.onClick.AddListener(HandleExitButtonClick);
+        }
+
+        private void OnDisable()
+        {
+            playButton.onClick.RemoveListener(HandlePlayButtonClick);
+            exitButton.onClick.RemoveListener(HandleExitButtonClick);
+        }
+
+        private void HandlePlayButtonClick()
+        {
+            GameEvents.RaiseLoadScene(playSceneCollection);
+        }
+
+        private static void HandleExitButtonClick()
+        {
+#if UNITY_EDITOR
+            EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
+        }
+    }
+}
