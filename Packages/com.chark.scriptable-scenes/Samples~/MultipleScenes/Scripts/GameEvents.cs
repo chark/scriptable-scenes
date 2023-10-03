309c9427587954f775ddff624de9bb77837615b8
Edvinas <edvinas108@gmail.com>
2023-10-03 20:37:21 +0300

    Restore sample

---
+++ Packages/com.chark.scriptable-scenes/Samples~/MultipleScenes/Scripts/GameEvents.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace CHARK.ScriptableScenes.Samples.ScenesAndCollections
+{
+    internal static class GameEvents
+    {
+        private static ScriptableSceneController sceneController;
+
+        internal static void RaiseReloadLoadedScene()
+        {
+            if (sceneController == false)
+            {
+                SetupSceneController();
+            }
+
+            sceneController.ReloadLoadedSceneCollection();
+        }
+
+        internal static void RaiseLoadScene(ScriptableSceneCollection collection)
+        {
+            if (sceneController == false)
+            {
+                SetupSceneController();
+            }
+
+            sceneController.LoadSceneCollection(collection);
+        }
+
+        private static void SetupSceneController()
+        {
+            sceneController = Object.FindObjectOfType<ScriptableSceneController>();
+        }
+    }
+}
